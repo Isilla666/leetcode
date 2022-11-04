955786835062ca42a85c3b8a0ccffb36351e7d8e
isilla <neverovau5@gmail.com>
2022-11-04 16:47:33 +0500

    add sol9

---
+++ LeetCode.CLI/Solution9.cs
@@ -0,0 +1,26 @@
+﻿//https://leetcode.com/problems/valid-anagram/
+public class Solution9 {
+    public bool IsAnagram(string s, string t)
+    {
+        var buff = new int['z'-'a'+1];
+        var indexBuff = new int[s.Length];
+        
+        if (s.Length != t.Length) return false;
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            buff[s[i] - 'a']++;
+            buff[t[i] - 'a']--;
+            indexBuff[i] = s[i] - 'a';
+        }
+
+        for (var i = 0; i < indexBuff.Length; i++)
+        {
+            if (buff[indexBuff[i]]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ LeetCode.Tests/UnitTest9.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace LeetCode.Tests;
+
+public class UnitTest9 : UnitTest<Solution9>
+{
+    [Test]
+    public void SpeedTest()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        /*for(var time = 0; time<1000000; time++)
+        {
+            task.IsAnagram("anagram", "nagaram");
+        }*/
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedTicks);
+        sw.Reset();
+        Assert.Pass();
+    }
+    [Test]
+    public void Test1()
+    {
+        var t = task.IsAnagram("anagram", "nagaram");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test2()
+    {
+        var t = task.IsAnagram("rat", "car");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test3()
+    {
+        var t = task.IsAnagram("zlap", "kcqx");
+        Assert.AreEqual(t, false);
+    }
+}
