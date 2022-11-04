f36ae09a2d5eafb577d77af9c4788ea5b5f25780
isilla <neverovau5@gmail.com>
2022-11-04 15:42:50 +0500

    add sol8

---
+++ LeetCode.CLI/Solution8.cs
@@ -0,0 +1,35 @@
+﻿//https://leetcode.com/problems/valid-parentheses/
+public class Solution8 {
+    public bool IsValid(string s)
+    {
+        var st = new Stack<char>();
+        for (var i = 0; i < s.Length; i++)
+        {
+            switch (s[i])
+            {
+                case '[':
+                    st.Push(s[i]);
+                    break;
+                case '{':
+                    st.Push(s[i]);
+                    break;
+                case '(':
+                    st.Push(s[i]);
+                    break;
+                case ']':
+                    if (st.Count == 0 || st.Peek() != '[') return false;
+                    st.Pop();
+                    break;
+                case '}':
+                    if (st.Count == 0 || st.Peek() != '{') return false;
+                    st.Pop();
+                    break;
+                case ')':
+                    if (st.Count == 0 || st.Peek() != '(') return false;
+                    st.Pop();
+                    break;
+            }
+        }
+        return st.Count==0;
+    }
+}
---
+++ LeetCode.Tests/UnitTest8.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+
+namespace LeetCode.Tests;
+
+public class UnitTest8 : UnitTest<Solution8>
+{
+    [Test]
+    public void SpeedTest()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        /*for(var time = 0; time<1000000; time++)
+        {
+            
+        }*/
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedTicks);
+        sw.Reset();
+        Assert.Pass();
+    }
+    [Test]
+    public void Test1()
+    {
+        var t = task.IsValid("()");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test2()
+    {
+        var t = task.IsValid("()[]{}");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test3()
+    {
+        var t = task.IsValid("(]");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test4()
+    {
+        var t = task.IsValid("()()()()");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test5()
+    {
+        var t = task.IsValid("}{");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test6()
+    {
+        var t = task.IsValid("}[)");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test7()
+    {
+        var t = task.IsValid("{}(]");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test8()
+    {
+        var t = task.IsValid("()[[");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test9()
+    {
+        var t = task.IsValid("()[]{}");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test10()
+    {
+        var t = task.IsValid("}})}({{]{");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test11()
+    {
+        var t = task.IsValid("()()[]{})(");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test12()
+    {
+        var t = task.IsValid("{}()()[](){}");
+        Assert.AreEqual(t, true);
+    }
+
+    [Test]
+    public void Test13()
+    {
+        var t = task.IsValid("][");
+        Assert.AreEqual(t, false);
+    }
+    [Test]
+    public void Test14()
+    {
+        var t = task.IsValid("([]){}");
+        Assert.AreEqual(t, true);
+    }
+    [Test]
+    public void Test15()
+    {
+        var t = task.IsValid("([)]");
+        Assert.AreEqual(t, false);
+    }
+}
