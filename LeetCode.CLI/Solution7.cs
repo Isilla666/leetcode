fe3982c0b308f0b61f981147325c932af1cab5b7
isilla <neverovau5@gmail.com>
2022-11-04 14:24:01 +0500

    Add solution 7

---
+++ LeetCode.CLI/Solution7.cs
@@ -0,0 +1,64 @@
+﻿//https://leetcode.com/problems/roman-to-integer/
+
+using System.Diagnostics;
+
+public class Solution7 {
+    
+    public int RomanToInt(string s)
+    {
+        var chars = new []{ 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+        var nums = new []{ 1, 5, 10, 50, 100, 500, 1000 };
+        var sum = 0;
+        var current = 0;
+        
+        for (var j = 0; j < chars.Length; j++)
+        {
+            if (s[0] == chars[j])
+            {
+                current = nums[j];
+                break;
+            }
+        }
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i == (s.Length - 1))
+            {
+                sum += current;
+                break;
+            }
+
+            var next = 0;
+            for (var j = 0; j < chars.Length; j++)
+            {
+                if (s[i+1] == chars[j])
+                {
+                    next = nums[j];
+                    break;
+                }
+            }
+            if (next>current)
+            {
+                sum += next - current;
+                i++;
+                if (i == (s.Length-1))
+                {
+                    break;
+                }
+                for (var j = 0; j < chars.Length; j++)
+                {
+                    if (s[i+1] == chars[j])
+                    {
+                        current = nums[j];
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                sum += current;
+                current = next;
+            }
+        }
+        return sum;
+    }
+}
---
+++ LeetCode.Tests/UnitTest7.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace LeetCode.Tests;
+
+public class UnitTest7 : UnitTest<Solution7>
+{
+    [Test]
+    public void SpeedTest()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        /*for(var time = 0; time<1000000; time++)
+        {
+            
+        }*/
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedTicks);
+        sw.Reset();
+        Assert.Pass();
+    }
+    [Test]
+    public void Test1()
+    {
+        var t = task.RomanToInt("III");
+        Assert.AreEqual(t, 3);
+    }
+    [Test]
+    public void Test2()
+    {
+        var t = task.RomanToInt("LVIII");
+        Assert.AreEqual(t, 58);
+    }
+    [Test]
+    public void Test3()
+    {
+        var t = task.RomanToInt("MCMXCIV");
+        Assert.AreEqual(t, 1994);
+    }
+    [Test]
+    public void Test4()
+    {
+        var t = task.RomanToInt("MCMXCV");
+        Assert.AreEqual(t, 1995);
+    }
+    [Test]
+    public void Test5()
+    {
+        var t = task.RomanToInt("MCMXCVI");
+        Assert.AreEqual(t, 1996);
+    }
+    [Test]
+    public void Test6()
+    {
+        var t = task.RomanToInt("MCMXC");
+        Assert.AreEqual(t, 1990);
+    }
+    [Test]
+    public void Test7()
+    {
+        var t = task.RomanToInt("MCMXV");
+        Assert.AreEqual(t, 1915);
+    }
+}
